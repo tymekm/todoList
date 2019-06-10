2a14683d0c8cfc9bda2b689e9cc7c7494e0ffbe7
tymekm <tymek.m@hotmail.com>
2019-06-10 17:05:33 +0200

    ready for deployment on heroku

--- todoList/settings.py
+++ todoList/settings.py
@@ -32,6 +32,21 @@ BOOTSTRAP3 = {
     'include_jquery': True
 }
 
+# Heroku settings
+cwd = os.getcwd()
+if cwd == '/app' or cwd[:4] == '/tmp':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    ALLOWED_HOSTS = ['*']
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    static_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static')
+    )
+
 # Application definition
 
 INSTALLED_APPS = [
